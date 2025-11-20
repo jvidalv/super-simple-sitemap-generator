d092393ecbab89db9ac2578db401742a714233d5
Josep Vidal <josepvidalvidal@gmail.com>
2025-11-20 15:19:22 +0100

    Remove dist folder from version control

--- dist/utils/timeout.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Creates a promise-based setTimeout wrapper
- * @param ms - Milliseconds to wait
- * @returns Promise that resolves after the specified time
- */
-export declare function timeout(ms: number): Promise<void>;
-//# sourceMappingURL=timeout.d.ts.map
